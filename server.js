e8b9f01a0ac5fa1d1e1afc2ee923d7eea52bd706
drohiro <rodrigorfsfr3@gmail.com>
2020-01-29 16:23:33 -0300

    Start

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Olar Mundo</p>
+    </body>
+    </html>
+    `)
+});
+server.listen(3000);
